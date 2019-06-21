ecc622bd89850fb738d3985d57dbb18e749ff629
Jarred Bultema <jarredbultema@gmail.com>
2019-06-21 14:15:05 -0600

    updated script

--- src/NN.py
+++ src/NN.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import classification_report
 
 import numpy as np
 import pandas as pd
@@ -17,6 +18,7 @@ def create_baseline():
 # create model
     model = Sequential()
     model.add(Dense(60, input_dim=155, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(30, input_dim=60, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -42,7 +44,10 @@ if __name__ == "__main__":
     np.random.seed(seed)
 
     # evaluate model with standardized dataset
-    estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=0)
+    estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=50, verbose=1)
     kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
     results = cross_val_score(estimator, X_train[:,:-1], y_train, cv=kfold)
-    print("Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+    y_predicted = model.predict(X_test)
+    test_score = classification_report(y_test, y_predicted)
+    print("CV Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+    print("Classification report: \n", test_score)
