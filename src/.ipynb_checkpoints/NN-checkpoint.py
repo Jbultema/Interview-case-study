33e92bad5fb501b9975395bd27b5f1c12d8f5cb0
Jarred Bultema <jarredbultema@gmail.com>
2019-06-21 13:50:24 -0600

    push to run on EC2

---
+++ src/.ipynb_checkpoints/NN-checkpoint.py
@@ -0,0 +1,48 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+import pandas as pd
+
+
+# baseline model
+def create_baseline():
+# create model
+    model = Sequential()
+    model.add(Dense(60, input_dim=155, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+if __name__ == "__main__":
+
+    df = pd.read_excel("data/cleaned_interview_data.xls")
+    df.drop(['Unnamed: 0'], axis= 1, inplace= True)
+
+    # create the columns to use in models
+    exclude_cols = ['expected_attendance_no','expected_attendance_yes', 'actual_attendance_yes']
+    include_cols = [x for x in df.columns.tolist() if x not in exclude_cols]
+    y = df['actual_attendance_yes'].values
+    X = df[include_cols].values
+
+    # split the data into train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    # fix random seed for reproducibility
+    seed = 7
+    np.random.seed(seed)
+
+    # evaluate model with standardized dataset
+    estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=0)
+    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+    results = cross_val_score(estimator, X_train[:,:-1], y_train, cv=kfold)
+    print("Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
